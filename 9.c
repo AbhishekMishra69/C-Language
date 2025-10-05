b4361f5dc97ee4b4e84bf9fc88d923468eed43e1
Abhishek Mishra <abhishekbnm79@gmail.com>
2025-10-06 00:21:58 +0530

    /

--- 9.c
+++ 9.c
@@ -1,1 +1,23 @@
 //Write a program in C to check whether a character is an alphabet or not
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    // Input a character from user
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Check if the character is an alphabet (A-Z or a-z)
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+    {
+        printf("'%c' is an alphabet.\n", ch);
+    }
+    else
+    {
+        printf("'%c' is NOT an alphabet.\n", ch);
+    }
+
+    return 0;
+}
