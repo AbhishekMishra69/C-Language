b950b6fff8eaa7b77cafd47c5ebdab664ea6d5b4
Abhishek Mishra <abhishekbnm79@gmail.com>
2025-11-21 00:46:54 +0530

    .

---
+++ demo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int rows, coef, space, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i = 0; i < rows; i++) {
+        // Print leading spaces
+        for(space = 1; space <= rows - i; space++)
+            printf("  ");
+
+        coef = 1;
+        for(j = 0; j <= i; j++) {
+            if(j == 0 || i == 0)
+                coef = 1;
+            else
+                coef = coef * (i - j + 1) / j;
+
+            printf("%4d", coef);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
